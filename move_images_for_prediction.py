681c5d2980e858c9f140e7a1a36b9c828489ec89
Patrick Pata <patrickrpata@gmail.com>
2025-07-31 14:22:01 -0700

    Update move_images_for_prediction.py

--- move_images_for_prediction.py
+++ move_images_for_prediction.py
@@ -32,9 +32,13 @@ def move_different_files(source_folder, destination_folder, reference_folder):
 # source_folder = r"C:\Users\patri\python_workspace\uvp6_darkedge_939_images\images_with_prosome" # UVP6 DE images with no lipid segmented
 # source_folder = r"C:\Users\patri\OneDrive - Université Laval\Laval_Postdoc\Laval-imaging-analysis\UVP6_amundsen2023\Calanus\100\processed" # UVP6 Amund 2023 part 1 that were not segmented
 # source_folder = r"C:\Users\patri\OneDrive - Université Laval\Laval_Postdoc\Laval-imaging-analysis\UVP6_amundsen2023\Calanus\100\images for prediction" # UVP6 Amundsen 2023 part2, all not segmented or processed
-source_folder = r"C:\Users\patri\OneDrive - Université Laval\Laval_Postdoc\Laval-imaging-analysis\UVP6_darkedge\export_5108_20250311_1406_Calanus_no_visible_lipid_sac\export_5108_20250311_1406\100" # UVP6 DE, Calanus not classified as with visible lipid sac
-destination_folder = r"C:\Users\patri\python_workspace\uvp6_de_amund23p2_predictions\images_for_prediction"
-reference_folder = r"C:\Users\patri\python_workspace\uvp6_de_amund23p1_lipid\data\segmentations_processed"
+# source_folder = r"C:\Users\patri\OneDrive - Université Laval\Laval_Postdoc\Laval-imaging-analysis\UVP6_darkedge\export_5108_20250311_1406_Calanus_no_visible_lipid_sac\export_5108_20250311_1406\100" # UVP6 DE, Calanus not classified as with visible lipid sac
+# destination_folder = r"C:\Users\patri\python_workspace\uvp6_de_amund23p2_predictions\images_for_prediction"
+# reference_folder = r"C:\Users\patri\python_workspace\uvp6_de_amund23p1_lipid\data\segmentations_processed"
+
+source_folder = r"C:\Users\patri\OneDrive - Université Laval\Laval_Postdoc\Laval-imaging-analysis\UVP6_amundsen2024\calanus_black_bg\leg5z\100"
+destination_folder = r"C:\Users\patri\python_workspace\uvp6_amund_2024_test"
+reference_folder = r"C:\Users\patri\OneDrive - Université Laval\Laval_Postdoc\Laval-imaging-analysis\UVP6_amundsen2024\calanus_black_bg\leg5z\100" # same as source to move all files
 
 move_different_files(source_folder, destination_folder, reference_folder)
 
